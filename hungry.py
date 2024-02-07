55a620a509e77cb8a2c09cada0ce772a68a9b35f
Shifa Naaz <naashifa2615@gmail.com>
2024-02-08 01:08:55 +0530

    simple

--- hungry.py
+++ hungry.py
@@ -1,13 +1,8 @@
 hungry=input('are u hungry?')
 if hungry=='yes':
     print('eat samosa')
-    print("pizza")
-    print("burger")
+
 else:
     print('do your work')
 
-    print("it is a main branch")
-
-    print("go for play")
-    print("thirsty")
 
